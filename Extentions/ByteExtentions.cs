5390bed23933a037af38402b761459225195a0ce
Divinity Softworks <divinitysoftworks@gmail.com>
2024-06-02 19:22:28 +0200

    Initial Commit.

---
+++ Extentions/ByteExtentions.cs
@@ -0,0 +1,20 @@
+﻿namespace System;
+
+/// <summary>
+/// Provides extension methods for the <see cref="byte[]"/> type.
+/// </summary>
+internal static class ByteExtentions {
+
+    /// <summary>
+    /// Converts a byte array to a Base64 URL encoded string.
+    /// </summary>
+    /// <param name="input">The byte array to encode.</param>
+    /// <returns>The Base64 URL encoded string.</returns>
+    public static string ToBase64UrlEncoded(this byte[] input) {
+        var output = Convert.ToBase64String(input);
+        output = output.Split('=')[0]; // Remove any trailing '='s
+        output = output.Replace('+', '-'); // 62nd char of encoding
+        output = output.Replace('/', '_'); // 63rd char of encoding
+        return output;
+    }
+}
