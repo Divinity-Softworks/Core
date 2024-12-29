95fea41afbd9f52a854713ed635c341fa4b1d774
Divinity Softworks <divinitysoftworks@gmail.com>
2024-12-29 13:52:51 +0100

    Made the return type a Task, because it is Async.

--- DivinitySoftworks.Core.Net.EventBus/IPublisher.cs
+++ DivinitySoftworks.Core.Net.EventBus/IPublisher.cs
@@ -1,16 +1,18 @@
 ﻿namespace DivinitySoftworks.Core.Net.EventBus;
 
 /// <summary>
-/// Defines a contract for publishing messages to an event bus.
+/// Defines a contract for asynchronously publishing messages to an event bus.
 /// </summary>
 public interface IPublisher {
     /// <summary>
     /// Publishes a message of type <typeparamref name="T"/> to the event bus
-    /// and returns a result of type <typeparamref name="R"/>.
+    /// and returns a result of type <typeparamref name="R"/> asynchronously.
     /// </summary>
     /// <typeparam name="T">The type of the message to publish.</typeparam>
     /// <typeparam name="R">The type of the result returned after publishing.</typeparam>
     /// <param name="message">The message to publish.</param>
-    /// <returns>A result of type <typeparamref name="R"/>.</returns>
-    R PublishAsync<T, R>(T message);
+    /// <returns>
+    /// A task representing the asynchronous operation, containing a result of type <typeparamref name="R"/>.
+    /// </returns>
+    Task<R> PublishAsync<T, R>(T message);
 }
