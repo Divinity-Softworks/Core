00bd6681cb813f64c9f8304335b24986aff42aa8
Divinity Softworks <divinitysoftworks@gmail.com>
2024-12-29 13:07:59 +0100

    Added an EventBus package.

---
+++ DivinitySoftworks.Core.Net.EventBus/IPublisher.cs
@@ -0,0 +1,16 @@
+﻿namespace DivinitySoftworks.Core.Net.EventBus;
+
+/// <summary>
+/// Defines a contract for publishing messages to an event bus.
+/// </summary>
+public interface IPublisher {
+    /// <summary>
+    /// Publishes a message of type <typeparamref name="T"/> to the event bus
+    /// and returns a result of type <typeparamref name="R"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the message to publish.</typeparam>
+    /// <typeparam name="R">The type of the result returned after publishing.</typeparam>
+    /// <param name="message">The message to publish.</param>
+    /// <returns>A result of type <typeparamref name="R"/>.</returns>
+    R PublishAsync<T, R>(T message);
+}
---
+++ DivinitySoftworks.Core.Net.EventBus/ISubscriber.cs
@@ -0,0 +1,7 @@
+﻿namespace DivinitySoftworks.Core.Net.EventBus;
+
+/// <summary>
+/// Defines a contract for subscribing to messages on an event bus.
+/// </summary>
+public interface ISubscriber {
+}
